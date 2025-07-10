2b10613aea44075582a6ffa9e695e06328b55181
LarissaRenew <76737414+LarissaRenew@users.noreply.github.com>
2025-07-10 14:43:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,8 @@ def home():
 
 @app.route("/abrir-chamado", methods=["POST"])
 def abrir_chamado():
-    # Apenas simulação de resposta da API Ellevo
-    dados_recebidos = request.get_json()
-    return jsonify({
-        "mensagem": "Chamado recebido com sucesso!",
-        "dados_recebidos": dados_recebidos
-    })
+    return jsonify({"mensagem": "A rota está funcionando!"})
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # para Render
